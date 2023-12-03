82c57b0a6471a788cf107ca58dc2271744b617b6
Joseph <josephdsumlin@gmail.com>
2023-12-03 00:59:15 -0500

    added flags to indicate which castles have been completed

--- app/src/main/java/com/example/fantasypuzzlegame/activities/HubActivity.java
+++ app/src/main/java/com/example/fantasypuzzlegame/activities/HubActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.fantasypuzzlegame.R;
 import com.example.fantasypuzzlegame.database.SaveState;
+
+import java.util.ArrayList;
 
 public class HubActivity extends AppCompatActivity {
 
@@ -19,6 +22,18 @@ public class HubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hub);
 //        TextView textView = findViewById(R.id.textView4);
 //        textView.setText("Save: " + SaveState.saveID+", "+SaveState.castleCompletions[0]+", "+SaveState.castleCompletions[1]);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ArrayList<ImageView> castleFlags = new ArrayList<>();
+        castleFlags.add(findViewById(R.id.castle1FlagImage));
+        castleFlags.add(findViewById(R.id.castle2FlagImage));
+        if(SaveState.castleCompletions[0]) castleFlags.get(0).setVisibility(View.VISIBLE);
+        else castleFlags.get(0).setVisibility(View.INVISIBLE);
+        if(SaveState.castleCompletions[1]) castleFlags.get(1).setVisibility(View.VISIBLE);
+        else castleFlags.get(1).setVisibility(View.INVISIBLE);
     }
 
     public void onCastle1Select(View view){
